1e5c4352e4685bd02c1784c09c1e81bf91f4fce6
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-04 14:33:01 +0600

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// const fetching data from the API;
+const API_KEY = `ba618ddea417393e6e47457e70072e86`;
+const weatherApi = async(city) =>{
+     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+     const res = await fetch(url);
+     const data = await res.json();
+     console.log(data);
+}
+
+// Calling the WeatherApi function;
+weatherApi('dhaka')
