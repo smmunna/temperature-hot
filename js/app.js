68e3ad5b91ba7d128da2f096df12910c576632f9
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-04 15:13:52 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,34 +1,34 @@
 // const fetching data from the API;
 const API_KEY = `ba618ddea417393e6e47457e70072e86`;
-const weatherApi = async(city) =>{
-     try {
+const weatherApi = async (city) => {
+    try {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-     const res = await fetch(url);
-     const data = await res.json();
+        const res = await fetch(url);
+        const data = await res.json();
 
-     const weatherStatus = document.getElementById('weatherStatus');
-     const inputValidation = document.getElementById('inputValidation');
+        const weatherStatus = document.getElementById('weatherStatus');
+        const inputValidation = document.getElementById('inputValidation');
 
-     if(city === data.name){
-        console.log('matched')
-        displayData(data);
-        
-        weatherStatus.classList.remove('d-none');
-        inputValidation.classList.add('d-none');
-     }
-     else{
-        // If the city not found with this name;
-        inputValidation.classList.remove('d-none');
-        weatherStatus.classList.add('d-none');
-     }
-     } catch (error) {
-        console.log('Error occurs with api'+error);
-     }
-     
+        if (city === data.name) {
+            console.log('matched')
+            displayData(data);
+
+            weatherStatus.classList.remove('d-none');
+            inputValidation.classList.add('d-none');
+        }
+        else {
+            // If the city not found with this name;
+            inputValidation.classList.remove('d-none');
+            weatherStatus.classList.add('d-none');
+        }
+    } catch (error) {
+        console.log('Error occurs with api' + error);
+    }
+
 }
 
 // DisplayWeather Data to the FrontEnd;
-const displayData = (weather)=>{
+const displayData = (weather) => {
     //console.log(weather)
     const city_name = weather.name;
     const temparature = weather.main.temp;
@@ -46,7 +46,7 @@ const displayData = (weather)=>{
 }
 
 //Searching by cityName;
-document.getElementById('searchCity').addEventListener('click',function(){
+document.getElementById('searchCity').addEventListener('click', function () {
     const cityInput = document.getElementById('citynameInput');
     const cityNameValue = cityInput.value;
     weatherApi(cityNameValue);
